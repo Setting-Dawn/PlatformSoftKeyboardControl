a6f1d660d1ff50e53d9d96bc9d50598f48f16309
Setting-Dawn <89761215+Setting-Dawn@users.noreply.github.com>
2025-12-09 15:11:30 -0800

    small change to board reflecting fixed SCL and SDA miswiring

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*! @file main.cpp
 *   This project was created to run the soft-sensor activated balancing platform
 *   as the final project of Cal Poly SLO Fall ME 507 class.
-*   @author Jake
+*   @author Jake Rowen
 *   @author Setting-Dawn
 *   @author Ethan Siahpush
 *   @version 2.0.0
